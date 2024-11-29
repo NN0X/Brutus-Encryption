6e7322e7b4cf4b81be5240a8f4650801ac9a6b43
nox <ts.sarkowicz@gmail.com>
2024-11-29 17:50:47 +0100

    changes

--- analizer.py
+++ analizer.py
@@ -5,22 +5,48 @@ import numpy as np
 import random
 import string
 import matplotlib.pyplot as plt
+import math
 
 def f(x, a=2**60, b=0.004, c=0.5):
     return -(a * np.exp(-b * (x - 64)**2) + c * x)
 
+def flip(f, line, axis='x'):
+    if axis == 'x':
+         return lambda x: f(2 * line - x)
+    elif axis == 'y':
+        return lambda x: 2 * line - f(x)
+    else:
+        raise ValueError("axis must be 'x' or 'y'")
+
+def fFlip(x):
+    return flip(f, 0, axis='y')(x)
+
 def doubleToUInt64(value):
-    return np.uint64(value)
+    value = int(value)
+    return value & 0xFFFFFFFFFFFFFFFF
+
+def addWithOverflow(a, b):
+    a = int(a)
+    b = int(b)
+    return (a + b) & 0xFFFFFFFFFFFFFFFF
+
+def multiplyWithOverflow(a, b):
+    a = int(a)
+    b = int(b)
+    return (a * b) & 0xFFFFFFFFFFFFFFFF
 
 def genPseudouniqueInteger(text):
     num = 0
     elements = []
     E = 2.71828182845904523536
     for i in range(len(text)):
-        integerComponent = np.power(E, i) * np.sin(i) * np.cos(i) + f(i) * np.sinh(i)
+        integerComponent = np.power(E, i) * np.sin(i) * np.cos(i) + fFlip(i) + np.sin(1 / (np.sqrt(i + 1)))
         integerComponent = doubleToUInt64(integerComponent)
-        elements.append(ord(text[i]) * integerComponent)
-        num += elements[i]
+        wordComponent = np.power(E, ord(text[i]))
+        wordComponent = doubleToUInt64(wordComponent)
+        elements.append(multiplyWithOverflow(wordComponent, integerComponent))
+        #elements.append(ord(text[i]) * integerComponent)
+        num = addWithOverflow(num, elements[-1])
     return num, elements
 
 def hashString(string):
@@ -48,6 +74,7 @@ def main():
                 pseudouniqueValues.add(pseudouniqueValue)
             else:
                 print(f"not unique: {word}")
+                input()
             allPseudouniqueValues.append(pseudouniqueValue)
     except KeyboardInterrupt:
         print("\nStopping word generation. Generating plot...")
@@ -85,12 +112,28 @@ def main():
         plt.title("Scatter Plot of Standard Deviation of Contribution of Elements to Pseudounique Values")
 
         x = np.linspace(0, 128)
-        y = f(x)
+        y = fFlip(x)
         plt.figure(figsize=(12, 8))
-        plt.plot(x, y, color='black')
+        plt.plot(x, y, color='orange')
         plt.xlabel("x")
         plt.ylabel("f(x)")
         plt.title("Plot of f(x) = 100 * exp(-0.01 * (x - 25)^2) + 0.5 * x")
+
+        # plot the process of adding elements to the pseudounique value for randomly selected 100 pseudounique values
+        # use function addWithOverflow to add elements to the pseudounique value
+        plt.figure(figsize=(12, 8))
+        for i in range(10):
+            randomIndex = random.randint(0, len(allPseudouniqueValues) - 1)
+            pseudouniqueValue = allPseudouniqueValues[randomIndex]
+            elements = elementsPerValue[randomIndex]
+            cumulativeSum = []
+            for j in range(len(elements)):
+                pseudouniqueValue = addWithOverflow(pseudouniqueValue, elements[j])
+                cumulativeSum.append(pseudouniqueValue)
+            plt.plot(range(len(cumulativeSum)), cumulativeSum, label=f"Pseudounique Value {randomIndex}")
+        plt.xlabel("Index of Element")
+        plt.ylabel("Cumulative Sum of Elements")
+        plt.title("Plot of Cumulative Sum of Elements for 10 Pseudounique Values")
 
         plt.show()
 

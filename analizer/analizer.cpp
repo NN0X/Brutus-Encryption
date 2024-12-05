3a09e43c25cfbb67c7f7091e7fde23883d2f4d26
nox <ts.sarkowicz@gmail.com>
2024-12-05 12:30:03 +0100

    analizer change

--- analizer/analizer.cpp
+++ analizer/analizer.cpp
@@ -2,9 +2,23 @@
 
 #include <iostream>
 
-int main()
+int main(int argc, char *argv[])
 {
-	int a = Brutus::Internal::encryptCharacter('a', 0, 0);
-	std::cout << a << std::endl;
+	if (argc < 2)
+	{
+		std::cerr << "Usage: " << argv[0] << " <word> <key>" << std::endl;
+		return 1;
+	}
+
+	std::string word = argv[1];
+	std::string key = argv[2];
+	unsigned long long int keyInt = Brutus::Internal::convertKey(key);
+	std::cout << keyInt << "\n";
+	
+	for (size_t i = 0; i < word.size(); i++)
+	{
+		std::cout << Brutus::Internal::encryptCharacter(word[i], keyInt, i) << " ";
+	}
+
 	return 0;
 }
--- analizer/analizer.py
+++ analizer/analizer.py
@@ -8,29 +8,13 @@ import matplotlib.pyplot as plt
 import math
 import subprocess
 
-def addWithOverflow(a, b):
-    # run add executable with a and b as arguments
-    # return the sum of a and b
-    # output: sum (unsigned long long int)
-    process = subprocess.Popen(["./add", str(a), str(b)], stdout=subprocess.PIPE)
-    output = process.communicate()[0].decode().strip()
-    return int(output)
 
-def genPseudouniqueInteger(text):
-    # run pseudounique executable with text as argument
-    # return the pseudounique integer value and the number of elements in the text
-    # output: pseudouniqueValue element1 element2 ... elementN (unsigned long long int)
-    # example: 123 4 5 6 7
-    process = subprocess.Popen(["./pseudouniq", text], stdout=subprocess.PIPE)
-    output = process.communicate()[0].decode().strip()
-    output = output.split()
-    pseudouniqueValue = int(output[0])
-    elements = [int(x) for x in output[1:]]
-    return pseudouniqueValue, elements
-
-def hashString(string):
-    hash = hashlib.sha512(string.encode()).hexdigest()
-    return hash
+def runAnalizerExec(word, key):
+    output = subprocess.run(["./analizer", word, key], stdout=subprocess.PIPE)
+    output = output.stdout.decode("utf-8").split("\n")
+    pseudouniqueInt = int(output[0])
+    charnums = list(map(int, output[1].split()))
+    return pseudouniqueInt, charnums
 
 def genRandomWord(length=8):
     return ''.join(random.choices(string.ascii_lowercase, k=length))
@@ -46,16 +30,15 @@ def main():
         elementsPerValue = []
         attempts = 1
         nonUnique = 0
+        word = "hello"
         while True and attempts < MAX_ATTEMPTS + 1:
-            word = genRandomWord()
-            hashed = hashString(word)
-            pseudouniqueValue, elems = genPseudouniqueInteger(hashed)
-            elementsPerValue.append(elems)
+            key = genRandomWord()
+            pseudouniqueValue, charnums = runAnalizerExec(word, key)
             if pseudouniqueValue not in pseudouniqueValues:
-                print(f"attempt: {attempts} | unique: {word}")
+                print(f"attempt: {attempts} | unique: {key}")
                 pseudouniqueValues.add(pseudouniqueValue)
             else:
-                print(f"attempt: {attempts} | not unique: {word}")
+                print(f"attempt: {attempts} | not unique: {key}")
                 nonUnique += 1
             allPseudouniqueValues.append(pseudouniqueValue)
             attempts += 1
@@ -72,47 +55,6 @@ def main():
         plt.xlabel("Index of Generated Value")
         plt.ylabel("Pseudounique Integer Value")
         plt.title("Scatter Plot of Pseudounique Values")
-
-        allPseudouniqueValues = np.array(allPseudouniqueValues)
-        elementsPerValue = np.array(elementsPerValue)
-        contributionsPerValue = []
-        for i in range(len(elementsPerValue)):
-            contributions = elementsPerValue[i] / allPseudouniqueValues[i]
-            contributionsPerValue.append(contributions)
-        contributionsPerValue = np.array(contributionsPerValue, dtype=np.float64)
-
-        plt.figure(figsize=(12, 8))
-        plt.scatter(range(len(contributionsPerValue[0])), np.mean(contributionsPerValue, axis=0), color='red', alpha=0.5)
-        plt.xlabel("Index of Element")
-        plt.ylabel("Average Contribution to Pseudounique Value")
-        plt.title("Scatter Plot of Average Contribution of Elements to Pseudounique Values")
-
-        plt.figure(figsize=(12, 8))
-        plt.scatter(range(len(contributionsPerValue[0])), np.median(contributionsPerValue, axis=0), color='green', alpha=0.5)
-        plt.xlabel("Index of Element")
-        plt.ylabel("Median Contribution to Pseudounique Value")
-        plt.title("Scatter Plot of Median Contribution of Elements to Pseudounique Values")
-
-        plt.figure(figsize=(12, 8))
-        plt.scatter(range(len(contributionsPerValue[0])), np.std(contributionsPerValue, axis=0), color='purple', alpha=0.5)
-        plt.xlabel("Index of Element")
-        plt.ylabel("Standard Deviation of Contribution to Pseudounique Value")
-        plt.title("Scatter Plot of Standard Deviation of Contribution of Elements to Pseudounique Values")
-
-        probe = 20
-        plt.figure(figsize=(12, 8))
-        for i in range(probe):
-            randomIndex = random.randint(0, len(allPseudouniqueValues) - 1)
-            pseudouniqueValue = allPseudouniqueValues[randomIndex]
-            elements = elementsPerValue[randomIndex]
-            cumulativeSum = []
-            for j in range(len(elements)):
-                pseudouniqueValue = addWithOverflow(pseudouniqueValue, elements[j])
-                cumulativeSum.append(pseudouniqueValue)
-            plt.plot(range(len(cumulativeSum)), cumulativeSum, label=f"Pseudounique Value {randomIndex}")
-        plt.xlabel("Index of Element")
-        plt.ylabel("Cumulative Sum of Elements")
-        plt.title(f"Plot of Cumulative Sum of Elements for {probe} Pseudounique Values")
 
         plt.show()
 
